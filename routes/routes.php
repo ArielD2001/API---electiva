00232c1c0accf46d46336b53bf0d42cc2b8c2768
Ariel Caraballo <ariel.cdiaz321@gmail.com>
2024-04-15 23:52:38 -0500

    cambio en route.php añadiendo la condicional si es metodo get

---
+++ routes/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+//Se toman los parametros que vienen en la URL
+$RouteArray = explode('/', $_SERVER['REQUEST_URI']);
+$RouteArray = array_filter($RouteArray);
+array_shift($RouteArray);
+array_shift($RouteArray);
+$table = explode('?', $RouteArray[0])[0];
+
+//Si no se esta haciendo ninguna peticion a la API
+if (count($RouteArray) == 0) {
+    $json = array(
+        "status" => 404,
+        "results" => 'Not found'
+    );
+
+    echo json_encode($json, http_response_code($json['status']));
+    return;
+}
+
+//Si se esta haciendo una peticion a la API
+if (count($RouteArray) == 1 && isset($_SERVER['REQUEST_METHOD'])) {
+
+    $peticion = $_SERVER['REQUEST_METHOD'];
+
+    //Peticiones GET
+    if ($peticion == 'GET') {
+        include('services/get.php');
+    }
+
+}
