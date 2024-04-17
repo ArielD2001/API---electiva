db447ce810d4aa04737def12361ec80d0233a935
Ariel Caraballo <ariel.cdiaz321@gmail.com>
2024-04-16 23:24:02 -0500

    cracion de los archivos post, configuracion de connecion y añadir cambios a metodo get

--- models/connection.php
+++ models/connection.php
@@ -1,9 +1,11 @@
 <?php
 
 
-class Connection{
-    
-    static public function infoDatabase(){
+class Connection
+{
+
+    static public function infoDatabase()
+    {
 
         $info = array(
 
@@ -16,19 +18,19 @@ class Connection{
         return $info;
     }
 
-    static public function connect(){
+    static public function connect()
+    {
 
-        try{
+        try {
 
             $conn = new PDO(
-                'mysql:host=localhost;dbname='.Connection::infoDatabase()['database'],
-                                               Connection::infoDatabase()['user'], 
-                                               Connection::infoDatabase()['pass']
+                'mysql:host=localhost;dbname=' . Connection::infoDatabase()['database'],
+                Connection::infoDatabase()['user'],
+                Connection::infoDatabase()['pass']
             );
 
-            $conn->exec('set names utf8');  
-
-        }catch (PDOException $e) {
+            $conn->exec('set names utf8');
+        } catch (PDOException $e) {
             print "¡Error!: " . $e->getMessage() . "<br/>";
             die();
         }
@@ -36,4 +38,32 @@ class Connection{
         return $conn;
     }
 
+    static public function getTable($table, $columns)
+    {
+
+        $database = Connection::infoDatabase()['database'];
+
+        if (in_array("*", $columns)) {
+
+            return Connection::connect()
+            ->query("SELECT COLUMN_NAME AS ITEM FROM information_schema.columns WHERE table_schema = '$database' AND table_name = '$table'")
+            ->fetchAll(PDO::FETCH_OBJ);
+
+        } else {
+            $validate = Connection::connect()
+                ->query("SELECT COLUMN_NAME AS ITEM FROM information_schema.columns WHERE table_schema = '$database' AND table_name = '$table'")
+                ->fetchAll(PDO::FETCH_OBJ);
+            $sum = 0;
+            if (empty($validate)) {
+                return null;
+            } else {
+                foreach ($validate as $key => $value) {
+                    $sum += (in_array($value->ITEM, $columns));
+                }
+                return $sum == count($columns) ? $validate : null;
+            }
+        }
+    }
+
+    
 }
